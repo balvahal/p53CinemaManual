8368b5c1cb20877a8fc5002be8e62ee9124adb91
Kyle Wayne Karhohs <gandalfisarockstar@gmail.com>
2014-04-10 03:07:37 -0400

    only one axes is needed in the imageViewer

--- p53CinemaManual_function_imageViewer_updateAnnotations.m
+++ p53CinemaManual_function_imageViewer_updateAnnotations.m
@@ -4,12 +4,13 @@ function [] = p53CinemaManual_function_imageViewer_updateAnnotations(master)
 %% Organize the relevant variables
 %
 
+if isempty(master.obj_imageViewer.pixelxy)
+    return
+end
+%% Organize the relevant variables
+%
+handles = guidata(master.obj_imageViewer.gui_imageViewer);
 %%
 %
-myCurrentPoint = master.obj_imageViewer.pixelxy;
-if ~isempty(myCurrentPoint)
-    %% add to data set
-    mystr = sprintf('x = %d\ny = %d',myCurrentPoint(1),myCurrentPoint(2));
-    disp(mystr);
-    drawnow;
-end
+
+set(handles.scatterPatch,'XData',rand(1,16)*master.image_width,'YData',rand(1,16)*master.image_height);
