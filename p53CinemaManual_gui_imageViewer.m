16d2666b00b8aea11fcec0359a01a040f489d132
ReyesJose <josereyes@fas.harvard.edu>
2014-04-21 22:26:41 -0400

    Cell tracking redesign

--- p53CinemaManual_gui_imageViewer.m
+++ p53CinemaManual_gui_imageViewer.m
@@ -156,88 +156,89 @@ set(f,'Visible','on');
 %%
 %
     function fWindowButtonDownFcn(source,event)
-        %%
-        % This if statement prevents multiple button firings from a single
-        % click event
-        %         if master.obj_imageViewer.isMyButtonDown
-        %             return
-        %         end
-        if master.debugmode
-            currentPoint = master.obj_imageViewer.getPixelxy;
-            if ~isempty(currentPoint)
-                mystr = sprintf('x = %d\ty = %d',currentPoint(1),currentPoint(2));
-                disp(mystr);
-            else
-                mystr = sprintf('OUTSIDE AXES!!!');
-                disp(mystr);
-            end
-        end
-        currentPoint = master.obj_imageViewer.getPixelxy;
-        if(~master.obj_imageViewer.obj_cellTracker.isTracking)
-            return;
-        end
-        
-        %         master.obj_imageViewer.isMyButtonDown = true;
-        
-        currentPoint = master.obj_imageViewer.getPixelxy;
-        if(isempty(currentPoint))
-            return;
-        end
-        % If the dataset has been preprocessed, perform tracking under
-        % "magnet mode"
-        if(master.obj_fileManager.preprocessMode)
-            lookupRadius = master.obj_imageViewer.obj_cellTracker.getDistanceRadius;
-            queryCentroid = master.obj_imageViewer.obj_cellTracker.centroidsLocalMaxima.getClosestCentroid(master.obj_imageViewer.currentTimepoint, fliplr(currentPoint), lookupRadius);
-        else
-            queryCentroid = fliplr(currentPoint);
-        end
-        % If this is the first time the user clicks after starting a new
-        % track, define the selected cell
-        if(master.obj_imageViewer.obj_cellTracker.firstClick)
-            lookupRadius = master.obj_imageViewer.obj_cellTracker.getDistanceRadius / 6;
-            [cellCentroid1, cell_id1] = master.obj_imageViewer.obj_cellTracker.centroidsTracks.getClosestCentroid(master.obj_imageViewer.currentTimepoint, queryCentroid, lookupRadius);
-            [cellCentroid2, cell_id2] = master.obj_imageViewer.obj_cellTracker.centroidsTracks.getClosestCentroid(master.obj_imageViewer.currentTimepoint, fliplr(currentPoint), lookupRadius);
-            if(~isempty(cell_id2))
-                master.obj_imageViewer.setSelectedCell(cell_id2);
-                queryCentroid = cellCentroid2;
-            elseif(~isempty(cell_id1))
-                master.obj_imageViewer.setSelectedCell(cell_id1);
-                queryCentroid = cellCentroid1;
-            else
-                master.obj_imageViewer.setSelectedCell(master.obj_imageViewer.obj_cellTracker.centroidsTracks.getAvailableCellId);
-            end
-            master.obj_imageViewer.obj_cellTracker.firstClick = 0;
-        end
-        
-        %% Set the centroids in selected cell and time
-        master.obj_imageViewer.obj_cellTracker.centroidsTracks.setCentroid(master.obj_imageViewer.currentTimepoint, master.obj_imageViewer.selectedCell, queryCentroid, 1);
-        % Move centroid if there was one in division or death events
-        if(master.obj_imageViewer.obj_cellTracker.centroidsDivisions.getValue(master.obj_imageViewer.currentTimepoint, master.obj_imageViewer.selectedCell))
-                master.obj_imageViewer.obj_cellTracker.centroidsDivisions.setCentroid(master.obj_imageViewer.currentTimepoint, master.obj_imageViewer.selectedCell, queryCentroid, 1);                
-        end
-        if(master.obj_imageViewer.obj_cellTracker.centroidsDeath.getValue(master.obj_imageViewer.currentTimepoint, master.obj_imageViewer.selectedCell))
-                master.obj_imageViewer.obj_cellTracker.centroidsDeath.setCentroid(master.obj_imageViewer.currentTimepoint, master.obj_imageViewer.selectedCell, queryCentroid, 1);                
-        end
-        
-        master.obj_imageViewer.obj_cellTracker.setAvailableCells;
-        
-        selectionType = get(master.obj_imageViewer.gui_imageViewer,'SelectionType');
-        if(strcmp(selectionType, 'alt'))
-            annotationType = master.obj_imageViewer.obj_cellTracker.cellFateEvent;
-            if(strcmp(annotationType, 'Division'))
-                master.obj_imageViewer.obj_cellTracker.centroidsDivisions.setCentroid(master.obj_imageViewer.currentTimepoint, master.obj_imageViewer.selectedCell, queryCentroid, 1);                
-            end
-            if(strcmp(annotationType, 'Death'))
-                master.obj_imageViewer.obj_cellTracker.centroidsDeath.setCentroid(master.obj_imageViewer.currentTimepoint, master.obj_imageViewer.selectedCell, queryCentroid, 1);                
-            end
-        end
-        
-        master.obj_imageViewer.setImage;
-        drawnow;
-        frameSkip = master.obj_imageViewer.obj_cellTracker.getFrameSkip;
-        master.obj_imageViewer.nextFrame;
-              
-        %         master.obj_imageViewer.isMyButtonDown = false;
+        master.obj_imageViewer.obj_cellTracker.triggerTracking(get(master.obj_imageViewer.gui_imageViewer,'SelectionType'));
+%         %%
+%         % This if statement prevents multiple button firings from a single
+%         % click event
+%         %         if master.obj_imageViewer.isMyButtonDown
+%         %             return
+%         %         end
+%         if master.debugmode
+%             currentPoint = master.obj_imageViewer.getPixelxy;
+%             if ~isempty(currentPoint)
+%                 mystr = sprintf('x = %d\ty = %d',currentPoint(1),currentPoint(2));
+%                 disp(mystr);
+%             else
+%                 mystr = sprintf('OUTSIDE AXES!!!');
+%                 disp(mystr);
+%             end
+%         end
+%         currentPoint = master.obj_imageViewer.getPixelxy;
+%         if(~master.obj_imageViewer.obj_cellTracker.isTracking)
+%             return;
+%         end
+%         
+%         %         master.obj_imageViewer.isMyButtonDown = true;
+%         
+%         currentPoint = master.obj_imageViewer.getPixelxy;
+%         if(isempty(currentPoint))
+%             return;
+%         end
+%         % If the dataset has been preprocessed, perform tracking under
+%         % "magnet mode"
+%         if(master.obj_fileManager.preprocessMode)
+%             lookupRadius = master.obj_imageViewer.obj_cellTracker.getDistanceRadius;
+%             queryCentroid = master.obj_imageViewer.obj_cellTracker.centroidsLocalMaxima.getClosestCentroid(master.obj_imageViewer.currentTimepoint, fliplr(currentPoint), lookupRadius);
+%         else
+%             queryCentroid = fliplr(currentPoint);
+%         end
+%         % If this is the first time the user clicks after starting a new
+%         % track, define the selected cell
+%         if(master.obj_imageViewer.obj_cellTracker.firstClick)
+%             lookupRadius = master.obj_imageViewer.obj_cellTracker.getDistanceRadius / 6;
+%             [cellCentroid1, cell_id1] = master.obj_imageViewer.obj_cellTracker.centroidsTracks.getClosestCentroid(master.obj_imageViewer.currentTimepoint, queryCentroid, lookupRadius);
+%             [cellCentroid2, cell_id2] = master.obj_imageViewer.obj_cellTracker.centroidsTracks.getClosestCentroid(master.obj_imageViewer.currentTimepoint, fliplr(currentPoint), lookupRadius);
+%             if(~isempty(cell_id2))
+%                 master.obj_imageViewer.setSelectedCell(cell_id2);
+%                 queryCentroid = cellCentroid2;
+%             elseif(~isempty(cell_id1))
+%                 master.obj_imageViewer.setSelectedCell(cell_id1);
+%                 queryCentroid = cellCentroid1;
+%             else
+%                 master.obj_imageViewer.setSelectedCell(master.obj_imageViewer.obj_cellTracker.centroidsTracks.getAvailableCellId);
+%             end
+%             master.obj_imageViewer.obj_cellTracker.firstClick = 0;
+%         end
+%         
+%         %% Set the centroids in selected cell and time
+%         master.obj_imageViewer.obj_cellTracker.centroidsTracks.setCentroid(master.obj_imageViewer.currentTimepoint, master.obj_imageViewer.selectedCell, queryCentroid, 1);
+%         % Move centroid if there was one in division or death events
+%         if(master.obj_imageViewer.obj_cellTracker.centroidsDivisions.getValue(master.obj_imageViewer.currentTimepoint, master.obj_imageViewer.selectedCell))
+%                 master.obj_imageViewer.obj_cellTracker.centroidsDivisions.setCentroid(master.obj_imageViewer.currentTimepoint, master.obj_imageViewer.selectedCell, queryCentroid, 1);                
+%         end
+%         if(master.obj_imageViewer.obj_cellTracker.centroidsDeath.getValue(master.obj_imageViewer.currentTimepoint, master.obj_imageViewer.selectedCell))
+%                 master.obj_imageViewer.obj_cellTracker.centroidsDeath.setCentroid(master.obj_imageViewer.currentTimepoint, master.obj_imageViewer.selectedCell, queryCentroid, 1);                
+%         end
+%         
+%         master.obj_imageViewer.obj_cellTracker.setAvailableCells;
+%         
+%         selectionType = get(master.obj_imageViewer.gui_imageViewer,'SelectionType');
+%         if(strcmp(selectionType, 'alt'))
+%             annotationType = master.obj_imageViewer.obj_cellTracker.cellFateEvent;
+%             if(strcmp(annotationType, 'Division'))
+%                 master.obj_imageViewer.obj_cellTracker.centroidsDivisions.setCentroid(master.obj_imageViewer.currentTimepoint, master.obj_imageViewer.selectedCell, queryCentroid, 1);                
+%             end
+%             if(strcmp(annotationType, 'Death'))
+%                 master.obj_imageViewer.obj_cellTracker.centroidsDeath.setCentroid(master.obj_imageViewer.currentTimepoint, master.obj_imageViewer.selectedCell, queryCentroid, 1);                
+%             end
+%         end
+%         
+%         master.obj_imageViewer.setImage;
+%         drawnow;
+%         frameSkip = master.obj_imageViewer.obj_cellTracker.getFrameSkip;
+%         master.obj_imageViewer.nextFrame;
+%               
+%         %         master.obj_imageViewer.isMyButtonDown = false;
     end
 
     function fWindowScrollWheelFcn(~,event)
