faf804e05e0d7eadc8dd1bbd9d215c10bb8b56e6
ReyesJose <josereyes@fas.harvard.edu>
2015-03-05 13:40:06 -0500

    centroidBackup

--- centroidBackup.m
+++ centroidBackup.m
@@ -4,7 +4,7 @@ files = {files(:).name};
 files = files(~cellfun(@isempty,regexp(files,'\.mat')));
 for i=1:length(files)
     load(fullfile(centroidPath, files{i}));
-    myTable = centroids2table_withDivisions(centroidsTracks, centroidsDivisions);
+    myTable = centroids2table_withCellFates(centroidsTracks, centroidsDivisions, centroidsDeath);
     outputFile = regexprep(files{i}, '\.mat', '.txt');
     writetable(myTable, fullfile(centroidPath, outputFile), 'Delimiter', '\t');
 end
