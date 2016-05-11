95172585f9c925d1f88d96591539abaa670b15a9
ReyesJose <josereyes@fas.harvard.edu>
2016-05-11 18:34:07 -0400

    Added the triangle method

--- SEGMENTATION_TriangleMethod.m
+++ SEGMENTATION_TriangleMethod.m
@@ -1,81 +1,56 @@
-function [level]=SEGMENTATION_TriangleMethod(lehisto)
-%     Triangle algorithm
-%     This technique is due to Zack (Zack GW, Rogers WE, Latt SA (1977), 
-%     "Automatic measurement of sister chromatid exchange frequency", 
-%     J. Histochem. Cytochem. 25 (7): 74153, )
-%     A line is constructed between the maximum of the histogram at 
-%     (b) and the lowest (or highest depending on context) value (a) in the 
-%     histogram. The distance L normal to the line and between the line and 
-%     the histogram h[b] is computed for all values from a to b. The level
-%     where the distance between the histogram and the line is maximal is the 
-%     threshold value (level). This technique is particularly effective 
-%     when the object pixels produce a weak peak in the histogram.
-
-%     Use Triangle approach to compute threshold (level) based on a
-%     1D histogram (lehisto). num_bins levels gray image. 
-
-%     INPUTS
-%         lehisto :   histogram of the gray level image
-%         num_bins:   number of bins (e.g. gray levels)
-%     OUTPUT
-%         level   :   threshold value in the range [0 1];
+%% cellularGPS_TriangleMethod
+% a threhold method that works particularly well when the image consists of
+% mostly background and a foreground, or a large peak (background) and a
+% long tail (foreground).
+%
+%   [trithresh] = cellularGPS_TriangleMethod(I)
+%
+%%% Input
+% * I: A grayscale image.
+%
+%%% Output:
+% * trithresh: the threshold value determined from the image.
+%
+%%% Detailed Description
+% There is no detailed description.
+%
+%%% Other Notes
 % 
-%     Dr B. Panneton, June, 2010
-%     Agriculture and Agri-Food Canada
-%     St-Jean-sur-Richelieu, Qc, Canad
-%     bernard.panneton@agr.gc.ca
+function [trithresh] = cellularGPS_TriangleMethod(I, outlierQuantile)
+%% Approximate histogram as triangle
+%%%
+% Create the histogram
+A=double(reshape(I,[],1));
+A = A(A <= quantile(A, outlierQuantile));
+[n,xout]=hist(A,100);
+%%%
+% Find the highest peak the histogram
+[c,ind]=max(n);
+%%%
+% Assume the long tail is to the right of the peak and envision a line from
+% the top of this peak to the end of the histogram.
+% The slope of this line, the hypotenuse, is calculated.
+x1=0;
+y1=c;
+x2=length(n)-ind;
+y2=n(end);
+m=(y2-y1)/(x2-x1); %The slope of the line
 
-
-%   Find maximum of histogram and its location along the x axis
-    if ~iscolumn(lehisto)
-        lehisto = lehisto';
-    end
-    num_bins = length(lehisto);
-    [h,xmax]=max(lehisto);
-    xmax=round(mean(xmax));   %can have more than a single value!
-    h=lehisto(xmax);
-    
-%   Find location of first and last non-zero values.
-%   Values<h/10000 are considered zeros.
-    indi=find(lehisto>h/10000);
-    fnz=indi(1);
-    lnz=indi(end);
-
-%   Pick side as side with longer tail. Assume one tail is longer.
-    lspan=xmax-fnz;
-    rspan=lnz-xmax;
-    if rspan>lspan  % then flip lehisto
-        lehisto=fliplr(lehisto');
-        a=num_bins-lnz+1;
-        b=num_bins-xmax+1;
-        isflip=1;
-    else
-        lehisto=lehisto';
-        isflip=0;
-        a=fnz;
-        b=xmax;
-    end
-    
-%   Compute parameters of the straight line from first non-zero to peak
-%   To simplify, shift x axis by a (bin number axis)
-    m=h/(b-a);
-    
-%   Compute distances
-    x1=0:(b-a);
-    y1=lehisto(x1+a);
-    beta=y1+x1/m;
-    x2=beta/(m+1/m);
-    y2=m*x2;
-    L=((y2-y1).^2+(x2-x1).^2).^0.5;
-
-%   Obtain threshold as the location of maximum L.    
-    level=find(max(L)==L);
-    level=a+mean(level);
-    
-%   Flip back if necessary
-    if isflip
-        level=num_bins-level+1;
-    end
-    
-    level=level/num_bins;
+%% Find the greatest distance
+% We are looking for the greatest distance betweent the histrogram and line
+% of the triangle via perpendicular lines The slope of all lines
+% perpendicular to the histogram hypotenuse is the negative reciprocal
+p=-1/m; %The slope is now the negative reciprocal
+%%%
+% We now have two slopes and two points for two lines. We now need to solve
+% this two-equation system to find their intersection, which can then be
+% used to calculate the distances
+iarray=(0:(length(n)-ind));
+L=zeros(size(n));
+for i=iarray
+intersect=(1/(m-p))*[-p,m;-1,1]*[c;n(i+ind)-p*i]; %intersect(1)= y coordinate, intersect(2)= x coordinate
+L(i+ind)=sqrt((intersect(2)-i)^2+(intersect(1)-n(i+ind))^2);
+end
+[~,ind2]=max(L);
+trithresh=xout(ind2);
 end
