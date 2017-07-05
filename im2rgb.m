a7e0d27352f4dc96a5d2b2a51dd3ea2d99ddb552
ReyesJose <josereyes@fas.harvard.edu>
2017-07-05 17:29:46 -0400

    im2rgb

---
+++ im2rgb.m
@@ -0,0 +1,6 @@
+function IM_rgb = im2rgb(IM)
+    %IM_normalized = imnormalize(IM);
+    IM_normalized = IM;
+    IM_rgb = horzcat(IM_normalized, IM_normalized, IM_normalized);
+    IM_rgb = reshape(IM_rgb, [size(IM), 3]);
+end
