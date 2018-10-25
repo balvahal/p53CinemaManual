527ae0c3648d26f27cc281662fc91095ff35a439
ReyesJose <josereyes@fas.harvard.edu>
2018-10-25 15:39:28 -0400

    Removed barwitherr functions

--- barwitherr_auto_std.m
+++
@@ -1,9 +0,0 @@
-function [] = barwitherr_auto_std(values, multiplier)
-    averages = zeros(length(values),1);
-    sem = zeros(length(values),1);
-    for i=1:length(values)
-        averages(i) = mean(values{i});
-        sem(i) = std(values{i});
-    end
-    barwitherr(sem, averages);
-end
